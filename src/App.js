19e675e45196e88b6589ea0296315c019768abbf
mfridberg <mats.fridberg@gmail.com>
2021-12-27 16:44:40 +0100

    login form

--- src/App.js
+++ src/App.js
@@ -11,18 +11,22 @@ export const App = () => {
     setAuthorized(val)
   }
 
-  if (!authorized) {
-    return (
-      <div className="flex justify-center">
-        <Signin auth={authorized} handleLogin={handleLogin} />
-        <a
-          className="text-blue-400 hover:text-sky-700 text-bold"
-          href="https://www.youtube.com/watch?v=xvFZjo5PgG0"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Tailwind test
-        </a>
+  if (!authorized){
+    return(
+      <div>
+        <div className="flex justify-center">
+          <Signin auth={authorized} handleLogin={handleLogin}/>
+        </div>
+        <div className="flex justify-center">
+          <a 
+            class="text-blue-400 hover:text-sky-700 text-bold" 
+            href="https://www.youtube.com/watch?v=xvFZjo5PgG0"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Tailwind test
+          </a>
+        </div>
       </div>
     )
   } else {
--- src/Signin.js
+++ src/Signin.js
@@ -1,9 +1,52 @@
-import { Button } from "antd";
+import { Button, Form, Input, Checkbox } from "antd";
 
 export const Signin = (props) => {
+  // return (
+  //   <div className="flex justify-center mt-2">
+  //     <Button type="primary" onClick={() => props.handleLogin(!props.auth)}>Login</Button>
+  //   </div>
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div className="flex justify-center mt-2">
-      <Button type="primary" onClick={() => props.handleLogin(!props.auth)}>Login</Button>
+    <div className="flex m-10 justify-center bg-white p-10 shadow rounded">
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
